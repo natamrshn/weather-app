85cc5c0e9c3b67b3d9c2b39f2ead380e18eaad8b
Natalie <116460095+natamrshn@users.noreply.github.com>
2023-05-21 19:43:22 +0300

    Add files via upload

---
+++ weather app/index.js
@@ -0,0 +1,44 @@
+let weather = {
+  apiKey: "577b24a392b139e55bdf241367358da9",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity " + humidity + " %";
+    document.querySelector(".wind").innerText = "Wind Speed " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+document.querySelector(".search").addEventListener("click", function () {
+  weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup", function (e) {
+  if (e.key === "Enter") {
+    weather.search();
+  }
+});
+
+weather.fetchWeather("Kyiv");
